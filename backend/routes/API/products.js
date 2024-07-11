e3eaa1860dffa942786297340b4b29a3a93f876c
Tyler Woyshner <tylerwoyshner@Tylers-MacBook-Pro.local>
2024-07-11 15:15:13 -0400

    started server

--- backend/routes/API/products.js
+++ backend/routes/API/products.js
@@ -1,4 +1,20 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const express = require("express");
+const router = express.Router();
+
+
+
 // GET all products
+router.get("/products", async (req, res) => {
+    try {
+        const products = await prisma.product.findMany();
+        res.status(200).json(products);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+    });
 
 // GET product by id
 
@@ -7,3 +23,5 @@
 // POST add new product (admin)
 
 // DELETE delete a product (admin)
+
+modeule.exports = router;
