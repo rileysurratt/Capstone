d089e7d9fc0aacb4eb28bb62146edcd7cf9088b8
rileysurratt <rileysurratt13@gmail.com>
2024-07-11 21:02:03 -0400

    finished products endpoint

--- backend/routes/API/products.js
+++ backend/routes/API/products.js
@@ -3,7 +3,7 @@ const prisma = new PrismaClient();
 const express = require('express');
 const router = express.Router();
 
-
+// AMD Ryzen 9 5900X
 
 // GET all products
 router.get("/products", async (req, res) => {
@@ -17,11 +17,80 @@ router.get("/products", async (req, res) => {
     });
 
 // GET product by id
+router.get("/products/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await prisma.product.findUnique({
+            where: {
+                id: parseInt(id)
+            }
+        })
+        if (!product) {
+            res.status(404).json({ error: "Product not found" });
+        }
+        res.status(201).json(product);
+    } catch {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error"})
+    }
+})
 
-// PUT update a product (admin)
+// PATCH update a product (admin)
+router.patch("/products/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, description, price, quantity } = req.body;
+        const product = await prisma.product.update({
+            where: {
+                id: parseInt(id)
+            },
+            data: {
+                name, description, price, quantity
+            }
+        })
+        if (!product) {
+            res.status(404).json({ error: "Product not found" });
+        }
+        res.status(201).json(product);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+})
 
 // POST add new product (admin)
+router.post("/products", async (req, res) => {
+    try {
+        const { name, description, price, quantity, categoryId } = req.body;
+        const product = await prisma.product.create({
+            data: {
+                name, description, price, quantity, categoryId
+            }
+        })
+        res.status(201).json({"message": "product added"});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+})
 
 // DELETE delete a product (admin)
+router.delete("/products/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await prisma.product.delete({
+            where: {
+                id: parseInt(id)
+            }
+        })
+        if (!product) {
+            res.status(404).json({ error: "Product not found" });
+        }
+        res.status(201).json({"message": "product deleted"});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+})
 
 module.exports = router;
