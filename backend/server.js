2621abecb97119d1a9d4db3b1588c58815379ba9
KELangsdorf <kelangsdorf@gmail.com>
2024-07-13 14:29:37 -0400

    added CORS to have both backend and front communicate

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,18 @@ const app = express();
 // uses this port from heroku (i think we're using render?) otherwise hosted on port: 8080 (can change)
 const PORT = process.env.PORT || 3000; //port number that vite runs on, change later
 require('dotenv').config();
+//CORS to allow localhost 3000 to communicate with the frontend
+const cors = require('cors');
 
 const productRoutes = require('./routes/API/products');
 
 const userRoutes = require('./routes/API/users');
 const authRoutes = require('./routes/API/auth');
 const category = require('./routes/API/category')
+
+//CORS to allow both localhosts to work in unison
+app.use(cors());
+
 app.use(express.json());
 
 app.use('/api', productRoutes);
--- client/src/components/Router/Login.jsx
+++ client/src/components/Router/Login.jsx
@@ -5,39 +5,129 @@ import { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Login() {
+  const [loggedOn, setLoggedOn] = useState(false);
+  const [defaultState, setdefaultState] = useState({
+    email: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setLoggedOn(true);
+    }
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setError(null);
+    try {
+      const response = await fetch(
+        "http://localhost:3000/api/auth/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: defaultState.email,
+            password: defaultState.password,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Login has failed.");
+      }
+
+      const data = await response.json();
+
+      if (data.token) {
+        setLoggedOn(true);
+        localStorage.setItem("token", data.token);
+        navigate("/");
+      } else {
+        throw new Error("Login has failed.");
+      }
+    } catch (error) {
+      setError(error);
+      setLoggedOn(false);
+      localStorage.removeItem("token");
+    }
+  };
+  //The function to handle input changes.
+  const handleChange = (e) => {
+    setdefaultState({
+      ...defaultState,
+      [e.target.name]: e.target.value,
+    });
+  };
+  //The function to LOGOUT (remove the local storage)
+  const handleLogout = () => {
+    setLoggedOn(false);
+    localStorage.removeItem("token");
+    navigate("/");
+  };
 
   return (
     <div className="loginform">
-      <form className="userForm">
-        <label>Email:</label><br />
-        <input
-        placeholder="EMAIL"
-        name="email"
-        type="email"
-        required
-        className="userForm"
-        />
-        <br />
-        <label>Password:</label>
-        <input
-        placeholder="PASSWORD"
-        name="password"
-        type="password"
-        required
-        className="userForm"
-        />
-      </form>
-      <Button variant="secondary" className="ml-2">
-        Login
-      </Button>
-      <Button
-        variant="secondary"
-        className="ml-2"
-        onClick={() => navigate(`/register`)}
-      >
-        Register New Account
-      </Button>
+      {!loggedOn ? (
+        <form onSubmit={handleSubmit} className="form-group custom-form">
+          <label htmlFor="email">Email</label>
+          <input
+          placeholder="email"
+            name="email"
+            type="email"
+            required
+            className="form-control"
+            value={defaultState.email}
+            onChange={handleChange}
+          />
+          <br />
+          <label htmlFor="password">Password</label>
+          <input
+          placeholder="password"
+            name="password"
+            type="password"
+            required
+            className="form-control"
+            value={defaultState.password}
+            onChange={handleChange}
+          />
+          <br />
+          <Button
+            type="submit"
+            variant="secondary"
+            color="primary"
+            className="w-full max-w-xs"
+          >
+            {loading ? "Logging in..." : "Login"}
+          </Button>
+          <Button
+            variant="secondary"
+            className="ml-2"
+            onClick={() => navigate(`/register`)}
+          >
+            Register New Account
+          </Button>
+        </form>
+      ) : (
+        <div style={{ marginTop: "20px" }}>
+          <h3>Welcome In!</h3>
+          <Button variant="secondary" onClick={() => navigate("/account")}>
+            My Account
+          </Button>
+          <Button variant="secondary" onClick={handleLogout}>
+            Logout
+          </Button>
+        </div>
+      )}
+      {error && <h2>{error.message}</h2>}
     </div>
   );
 }
