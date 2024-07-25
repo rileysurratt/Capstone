8ccd90fee1adc6255fb1ebd3d80b40d474782326
Tyler Woyshner <tylerwoyshner@Tylers-MacBook-Pro.local>
2024-07-25 19:15:44 -0400

    added a checkout confirmation page

--- client/src/components/Router/Cart.jsx
+++ client/src/components/Router/Cart.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const Cart = () => {
   const [cartItems, setCartItems] = useState([]);
   const [total, setTotal] = useState(0);
+  const navigate = useNavigate();
+
 
   const calculateTotal = (items) => {
     const total = items.reduce((sum, item) => sum + (item.product.price * item.quantity), 0);
@@ -124,8 +127,12 @@ const Cart = () => {
       )}
       <h2>Total: ${total.toFixed(2)}</h2>
       {cartItems.length > 0 && (
+        <div>
         <button onClick={clearCart}>Clear Cart</button>
+        <button onClick={() => navigate('/checkout', { state: { cartItems, total } })}>Checkout</button>
+        </div>
       )}
+
     </div>
   );
 };
