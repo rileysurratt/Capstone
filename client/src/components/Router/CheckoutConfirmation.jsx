f26129ecb45638fde25b4946e2817225521e4e3c
Tyler Woyshner <tylerwoyshner@Tylers-MacBook-Pro.local>
2024-07-25 20:07:57 -0400

    checkout backend updated

--- client/src/components/Router/CheckoutConfirmation.jsx
+++ client/src/components/Router/CheckoutConfirmation.jsx
@@ -1,10 +1,36 @@
 import React from "react";
+import axios from 'axios';
 import { useLocation, useNavigate } from "react-router-dom";
 
 const CheckoutConfirmation = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const { cartItems, total } = location.state || { cartItems: [], total: 0 };
+
+  const handleCheckout = async () => {
+    try {
+      const token = localStorage.getItem('token');
+
+      const response = await axios.post(
+        'http://localhost:3000/api/checkout',
+        { cartItems, total },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+          withCredentials: true,
+        }
+      );
+
+      if (response.data.success) {
+        console.log('Order successful!')
+        navigate('/order-success'); 
+      } else {
+        console.error('Checkout failed:', response.data);
+      }
+    } catch (error) {
+      console.error('Error during checkout:', error);
+    }
+  };
+
 
   return (
     <div>
@@ -21,7 +47,7 @@ const CheckoutConfirmation = () => {
         <p>Your cart is empty</p>
       )}
       <h2>Total: ${total.toFixed(2)}</h2>
-      <button>Checkout!</button>
+      <button onClick={handleCheckout}>Checkout!</button>
       <button onClick={() => navigate('/cart')}>Back to Cart</button>
     </div>
   );
