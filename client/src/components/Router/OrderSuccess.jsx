9a184c494b004f10e8b99dbefe948985d168fa0c
Tyler Woyshner <tylerwoyshner@Tylers-MacBook-Pro.local>
2024-08-14 19:03:49 -0600

    added css final touches

--- client/src/components/Router/OrderSuccess.jsx
+++ client/src/components/Router/OrderSuccess.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import confetti from "canvas-confetti";
 import { useNavigate } from "react-router-dom";
+import './OrderSuccess.css';
+
 
 const OrderSuccess = () => {
   const navigate = useNavigate();
@@ -15,10 +17,10 @@ const OrderSuccess = () => {
   }, []);
 
     return (
-      <div>
+      <div className="order-success-container">
         <h1>Order Successful!</h1>
         <p>Thank you for your purchase. Your order is on its way!</p>
-        <button onClick={() => navigate("/catalog")}>Return to Products</button>
+        <button className="checkout-button" onClick={() => navigate("/catalog")}>Return to Products</button>
       </div>
     );
   };
